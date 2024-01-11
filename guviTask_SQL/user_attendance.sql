4a4e6c4f75a0034af21ef0832434188c089a79f1
RAJALAKSHMI <143245107+RAJALAKSHM22@users.noreply.github.com>
2024-01-11 21:02:21 +0530

    Add files via upload

---
+++ guviTask_SQL/user_attendance.sql
@@ -0,0 +1,13 @@
+insert into user_attendance value
+(1, "Yes", "non", 0,1),
+(2, "Yes", "Submited", 9,2),
+(3, "No", "Submited", 10,3),
+(4, "Yes", "Submited", 7,4),
+(5, "Yes", "partially",5,5),
+(6, "No", "No", 0,6),
+(7, "Yes", "Submited", 7,7),
+(8, "Yes", "Submited", 7,8),
+(9, "No", "Submited", 6,9),
+(10, "Yes", "Partially", 5,10),
+(11, "Yes", "Submited", 9,11),
+(12, "Yes", "Submited", 8,12);
---
+++ guviTask_SQL/zenClass.sql
@@ -0,0 +1,15 @@
+insert into zen_class values
+(1, STR_TO_DATE('01-01-2023', '%d-%m-%Y'), "Introduction", 36, 0 ,"Intro to web browser, dom, zen_classcontentHTML, CSS,JS ,node JS, Data Type, Array and Object"),
+(2, STR_TO_DATE("03-01-23", '%d-%m-%Y'), " Request and response",36,2,"JSON iteration, XMLHTTP request, window object"),
+(3, STR_TO_DATE("05-01-23", '%d-%m-%Y'), "Function", 36, 3, "hoisting, call by value and referance, types of functions"),
+(4, STR_TO_DATE("07-01-23", '%d-%m-%Y'), "ECMA script", 31, 1, "spread & rest operators,array & object destructure,Object shorthand, Template literals"),
+(5, STR_TO_DATE("09-01-23", '%d-%m-%Y'), "OOPs", 36, 1,"What are classes in js?, constructor, use of this keyword"),
+(6, STR_TO_DATE("10-01-23", '%d-%m-%Y'), "Array methods", 34, 3, " for-loop, Map, Reduce,  Filter"),
+(7, STR_TO_DATE("11-01-23", '%d-%m-%Y'), "Intro to HTML", 33, 1, "div vs section vs article, inline vs block elements, HTML Forms, Input elements and attributes"),
+(8, STR_TO_DATE("13-01-23", '%d-%m-%Y'), "Intro for CSs", 36, 1, "What is css?, ways to use css , roperty & value class and id, display property: inline, block,"),
+(9, STR_TO_DATE("15-01-23", '%d-%m-%Y'), "CSS", 36, 1, "box model, Flex property, position property, parent inherit, gradient"),
+(10,STR_TO_DATE("19-01-23", '%d-%m-%Y'), "CSS property", 34, 2, "overflow, dropbox, icons, media query"),
+(11,STR_TO_DATE("25-01-23", '%d-%m-%Y'), "Web Design", 35, 2, "responsive web design,BootStrap, Intro to DOM" ),
+(12,STR_TO_DATE("31-01-23", '%d-%m-%Y'), "Dom", 36,2,"createElement,setAttribute, getElementById, querySelector vs querySelectorAll,innerHTML vs innerText,appendChild vs append");
+
+-- SET SQL_SAFE_UPDATE=0;
